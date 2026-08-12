c1c6b6ab8282129b8f68d13b1ed4e84b1a203c2b
Diogo-Serra <di-code@outlook.com>
2026-08-12 12:48:13 +0100

    updates and fixes on environment and dependencies and adding API_KEY connection to HFhub

--- test_1.py
+++ test_1.py
@@ -1,10 +1,17 @@
+from os import environ
 from src import llm_sdk
+from dotenv import load_dotenv
+
+# Look for HuggingFace token
+load_dotenv()
+HF_TOKEN = environ.get("HF_TOKEN")
 
 
+# Test 1
 def main():
     llm = llm_sdk.Small_LLM_Model()
-    print("Ok")
-    print(llm._model_name)
+    print(f"Local LLM model name: {llm._model_name}")
+    print("Dependencies: Ok")
 
 
 if __name__ == "__main__":
