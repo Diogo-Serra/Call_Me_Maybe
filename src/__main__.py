be38784717490737b67fb74859e3f7edf3d1be13
Diogo-Serra <di-code@outlook.com>
2026-09-18 14:46:40 +0100

    dependecies import fixes

--- src/__main__.py
+++ src/__main__.py
@@ -22,7 +22,7 @@ def main() -> int:
         print("Preparing LLM model and resolving prompts... "
               "It can take sometime.")
         llm = llm_sdk.Small_LLM_Model(model_name=args.model)
-        print(f"Model in use: {llm._model_name}")
+        print(f"Model in use: {args.model}")
         engine = FunctionCallEngine(
             llm=llm,
             definitions_path=definitions_path,
